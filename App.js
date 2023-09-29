064def9dcd563003d1d3e1806cfdb5c18bd970b7
ships09 <ships09@gmail.com>
2023-09-29 21:13:21 +0530

    test

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-const heading1 = React.createElement("h1", {id:"heading", className: "heading t1"}, "Hello World from React!");
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", {id:"heading", className: "heading t1"}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const elem = React.createElement("div", {id: "parent"}, 
     React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "h1"}, "I am h1 tag"), React.createElement("h2", {id: "h2"}, "I am h2 tag"),]
     )
 ) 
-root1.render(elem);
+root.render(elem);
 console.log(elem);
