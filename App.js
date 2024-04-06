fc6b5355d473c5a853528fa7825b2ef1d3bb3ed5
ships09 <ships09@gmail.com>
2024-04-06 07:36:07 +0530

    Chapter 1 & 2

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-const heading = React.createElement("h1", {id:"heading", className: "heading t1"}, "Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading = React.createElement("h1", {id:"heading", className: "heading t1"}, "Hello World from Namaste React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const elem = React.createElement("div", {id: "parent"}, 
-    React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "h1"}, "I am h1 tag"), React.createElement("h2", {id: "h2"}, "I am h2 tag"),]
+    React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "h1"}, "I am h1 tag in namste"), React.createElement("h2", {id: "h2"}, "I am h2 tag"),]
     )
 ) 
 root.render(elem);
-console.log(elem);
+
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+/*const div = React.createElement("h1", {id:"shilpa_h1", className:"h1 heading"}, "Shilpa's Heading");
+root.render(div);*/
