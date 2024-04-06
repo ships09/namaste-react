edef15c9e24f6c7256ec12b02156e8971f3eefd6
ships09 <ships09@gmail.com>
2024-04-06 23:50:06 +0530

    Episode 3

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("h1", {id:"heading", className: "heading t1"}, "Hello World from Namaste React!");
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading1 = React.createElement("h1", {id: "heading", class:"heading"}, "Namaste React");
+//root.render(heading1);
+//JSX is html like or xml like syntax
+//React Element
+const heading = (<h1 id="heading" className="heading" tabIndex="1">Namaste from JSX</h1>);
 
-const elem = React.createElement("div", {id: "parent"}, 
-    React.createElement("div", {id: "child"}, [React.createElement("h1", {id: "h1"}, "I am h1 tag in namste"), React.createElement("h2", {id: "h2"}, "I am h2 tag"),]
+//React Functional Component
+const Title = () => (
+
+    <h1 className="heading"> Namaste React from JSX</h1>
+)
+//This is also a valid code
+const TitleOne = function () {
+    return (
+        <div id="subcontainer"><h1  > Namaste React from JSX</h1></div>
     )
-) 
-root.render(elem);
-
-
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-/*const div = React.createElement("h1", {id:"shilpa_h1", className:"h1 heading"}, "Shilpa's Heading");
-root.render(div);*/
+}
+const numericvalue = 99;
+const HeadingComponent = () => (
+    <div id="container">
+    <TitleOne></TitleOne>
+    <TitleOne />
+    <Title />
+    {numericvalue}
+    {console.log('shilpa')}
+    {Title()}
+    <h1 id="heading" className="heading">Namaste React from Functional Component</h1>
+    </div>
+)
+root.render(<HeadingComponent />);
